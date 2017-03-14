7d97c0e020955e87f2678c8f8c537421ddb835d3
CongBao <bao_cong@outlook.com>
2017-03-14 20:04:56 +0000

    add service layer javadoc

--- MovieRecommenderSystem/src/online/mrsys/movierecommender/service/MovieManager.java
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/service/MovieManager.java
@@ -8,37 +8,153 @@ import online.mrsys.movierecommender.domain.User;
 import online.mrsys.movierecommender.vo.MovieBean;
 
 public interface MovieManager {
-	
+
+	/**
+	 * Add a movie record to database.
+	 * 
+	 * @param movie
+	 *            the movie record to be added
+	 * @return if the operation succeed
+	 */
 	boolean addMovie(Movie movie);
-	
+
+	/**
+	 * Add a rating record to database.
+	 * 
+	 * @param rating
+	 *            the rating record to be added
+	 * @return if the operation succeed
+	 */
 	boolean addRating(Rating rating);
 
+	/**
+	 * Check if a movie is exist in database.
+	 * 
+	 * @param movie
+	 *            the movie to be checked
+	 * @return if the movie is exist in database
+	 */
 	boolean isMovieExist(Movie movie);
-	
+
+	/**
+	 * Check if a rating record is exist in database.
+	 * 
+	 * @param rating
+	 *            the rating record to be checked
+	 * @return if the rating record is exist in database
+	 */
 	boolean isRatingExist(Rating rating);
-	
+
+	/**
+	 * Update the year record of a movie.
+	 * 
+	 * @param origin
+	 *            the movie to be updated
+	 * @param year
+	 *            the new year of movie
+	 * @return the movie record after updating
+	 */
 	Movie updateYear(Movie origin, int year);
-	
+
+	/**
+	 * Update the title record of a movie.
+	 * 
+	 * @param origin
+	 *            the movie to be updated
+	 * @param title
+	 *            the new title of movie
+	 * @return the movie record after updating
+	 */
 	Movie updateTitle(Movie origin, String title);
-	
+
+	/**
+	 * Update the rating value of a rating record
+	 * 
+	 * @param origin
+	 *            the rating record to be updated
+	 * @param rating
+	 *            the new rating value of the rating record
+	 * @return the rating record after updating
+	 */
 	Rating updateRating(Rating origin, float rating);
-	
+
+	/**
+	 * Obtain a movie record by the movie's id.
+	 * 
+	 * @param id
+	 *            the id of movie
+	 * @return the movie record obtained
+	 */
 	Movie getMovieById(int id);
-	
+
+	/**
+	 * Obtain a movie bean instance by the movie's id.
+	 * 
+	 * @param id
+	 *            the id of movie
+	 * @return the movie bean instance obtained
+	 */
 	MovieBean getMovieBeanById(int id);
-	
+
+	/**
+	 * Obtain a list of movie records by year.
+	 * 
+	 * @param year
+	 *            the year of movie
+	 * @return a list of movie records obtained
+	 */
 	List<Movie> getMoviesByYear(int year);
-	
+
+	/**
+	 * Obtain a list of movie records by title.
+	 * 
+	 * @param title
+	 *            the title of movie
+	 * @return a list of movie records obtained
+	 */
 	List<Movie> getMoviesByTitle(String title);
-	
+
+	/**
+	 * Recommend a list of movies to a user.
+	 * 
+	 * @param user
+	 *            the user to be recommended
+	 * @return a list of recommended movies
+	 */
 	List<Movie> recommendMoviesToUser(User user); // call *.function.MovieRecommender to process
-	
+
+	/**
+	 * Obtain a list of rating records of a user.
+	 * 
+	 * @param user
+	 *            the user to be queried
+	 * @return a list of rating records obtained
+	 */
 	List<Rating> getRatingsByUser(User user);
-	
+
+	/**
+	 * Obtain a list of rating records of a movie.
+	 * 
+	 * @param movie
+	 *            the movie to be queried
+	 * @return a list of rating records obtained
+	 */
 	List<Rating> getRatingsByMovie(Movie movie);
-	
+
+	/**
+	 * Delete a movie record in database
+	 * 
+	 * @param movie
+	 *            the movie record to be deleted
+	 */
 	void deleteMovie(Movie movie);
-	
+
+	/**
+	 * Delete a rating record in database.
+	 * 
+	 * @param rating
+	 *            the rating record to be deleted
+	 */
 	void deleteRating(Rating rating);
-	
+
 }
--- MovieRecommenderSystem/src/online/mrsys/movierecommender/service/UserManager.java
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/service/UserManager.java
@@ -9,45 +9,175 @@ import online.mrsys.movierecommender.vo.RoleBean;
 import online.mrsys.movierecommender.vo.UserBean;
 
 public interface UserManager {
-	
+
 	int LOGIN_FAIL = -1;
 	int LOGIN_USER = 0;
 	int LOGIN_ADMIN = 1;
-	
+
 	int REGISTER_FAIL = -1;
 	int REGISTER_USER = 0;
 	int REGISTER_ADMIN = 1;
 
+	/**
+	 * Check if a login request if valid.
+	 * 
+	 * @param user
+	 *            the user to be checked
+	 * @return a flag of result, where
+	 *         <ul>
+	 *         <li>LOGIN_FAIL = -1</li>
+	 *         <li>LOGIN_USER = 0</li>
+	 *         <li>LOGIN_ADMIN = 1</li>
+	 *         </ul>
+	 */
 	int validLogin(User user);
-	
+
+	/**
+	 * Check if a register request is valid.
+	 * 
+	 * @param user
+	 *            the user to be checked
+	 * @return a flag of result, where
+	 *         <ul>
+	 *         <li>REGISTER_FAIL = -1</li>
+	 *         <li>REGISTER_USER = 0</li>
+	 *         <li>REGISTER_ADMIN = 1</li>
+	 *         </ul>
+	 */
 	int validRegister(User user);
-	
+
+	/**
+	 * Add a favorite record to database.
+	 * 
+	 * @param favorite
+	 *            the favorite record to be added
+	 * @return if the operation succeed
+	 */
 	boolean addFavorite(Favorite favorite);
-	
+
+	/**
+	 * Check if a user is exist in database.
+	 * 
+	 * @param user
+	 *            the user to be checked
+	 * @return if the user is exist in database
+	 */
 	boolean isUserExist(User user);
-	
+
+	/**
+	 * Update the account record of a user.
+	 * 
+	 * @param origin
+	 *            the user to be updated
+	 * @param account
+	 *            the new account of user
+	 * @return the user record after updating
+	 */
 	User updateAccount(User origin, String account);
-	
+
+	/**
+	 * Update the password record of a user.
+	 * 
+	 * @param origin
+	 *            the user to be updated
+	 * @param password
+	 *            the new password of user
+	 * @return the user record after updating
+	 */
 	User updatePassword(User origin, String password);
-	
+
+	/**
+	 * Update the email record of a user.
+	 * 
+	 * @param origin
+	 *            the user to be updated
+	 * @param email
+	 *            the new email of user
+	 * @return the user record after updating
+	 */
 	User updateEmail(User origin, String email);
-	
+
+	/**
+	 * Update the state of mail verification of a user.
+	 * 
+	 * @param origin
+	 *            the user to be updated
+	 * @param verified
+	 *            the new mail verify state of user
+	 * @return the user record after updating
+	 */
 	User updateMailVerifyState(User origin, boolean verified);
-	
+
+	/**
+	 * Update the role of a user.
+	 * 
+	 * @param origin
+	 *            the user to be updated
+	 * @param roleId
+	 *            the new role id of user
+	 * @return the user record after updating
+	 */
 	User updateRole(User origin, int roleId);
-	
+
+	/**
+	 * Obtain a user record by the user's account.
+	 * 
+	 * @param account
+	 *            the account of user
+	 * @return the user record obtained
+	 */
 	User getUserByAccount(String account);
-	
+
+	/**
+	 * Obtain a role record by the user's account.
+	 * 
+	 * @param account
+	 *            the account of user
+	 * @return the role record obtained
+	 */
 	Role getRoleByAccount(String account);
-	
+
+	/**
+	 * Obtain a user bean instance by the user's account.
+	 * 
+	 * @param account
+	 *            the account of user
+	 * @return the user bean instance obtained
+	 */
 	UserBean getUserBeanByAccount(String account);
-	
+
+	/**
+	 * Obtain a role bean instance by the user's account.
+	 * 
+	 * @param account
+	 *            the account of user
+	 * @return the role bean instance obtained
+	 */
 	RoleBean getRoleBeanByAccount(String account);
-	
+
+	/**
+	 * Obtain a list of favorite records of a user.
+	 * 
+	 * @param user
+	 *            the user to be queried
+	 * @return a list of favorite records obtained
+	 */
 	List<Favorite> getFavoritesByUser(User user);
-	
+
+	/**
+	 * Delete a user record in database.
+	 * 
+	 * @param user
+	 *            the user to be deleted
+	 */
 	void deleteUser(User user);
-	
+
+	/**
+	 * Delete a favorite record in database.
+	 * 
+	 * @param favorite
+	 *            the favorite record to be deleted
+	 */
 	void deleteFavorite(Favorite favorite);
-	
+
 }
