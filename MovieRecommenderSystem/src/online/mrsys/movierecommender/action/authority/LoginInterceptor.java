da9837e3465904d7699d83a5a004ab14cb33a421
CongBao <bao_cong@outlook.com>
2017-03-30 23:42:42 +0100

    add authority class

---
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/action/authority/LoginInterceptor.java
@@ -0,0 +1,14 @@
+package online.mrsys.movierecommender.action.authority;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class LoginInterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
