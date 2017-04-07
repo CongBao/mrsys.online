fd81e6da2f8e8dc256871ee3c4e6fc7ddfdb1de2
CongBao <bao_cong@outlook.com>
2017-04-08 00:50:51 +0100

    update struts config

---
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/action/LogoutAction.java
@@ -0,0 +1,13 @@
+package online.mrsys.movierecommender.action;
+
+import online.mrsys.movierecommender.action.base.BaseAction;
+
+public class LogoutAction extends BaseAction {
+
+    @Override
+    public String execute() throws Exception {
+        // TODO Auto-generated method stub
+        return super.execute();
+    }
+    
+}
---
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/action/user/LoadProfileAction.java
@@ -0,0 +1,23 @@
+package online.mrsys.movierecommender.action.user;
+
+import online.mrsys.movierecommender.action.base.BaseAction;
+
+public class LoadProfileAction extends BaseAction {
+    
+    private String account;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        // TODO Auto-generated method stub
+        return super.execute();
+    }
+    
+}
