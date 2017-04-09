e3c8c7747c7550d66ad12724ac5c665f2c408d66
CongBao <bao_cong@outlook.com>
2017-04-09 10:07:24 +0100

    add movie action

---
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/action/movie/LoadMovieAction.java
@@ -0,0 +1,23 @@
+package online.mrsys.movierecommender.action.movie;
+
+import online.mrsys.movierecommender.action.base.BaseAction;
+
+public class LoadMovieAction extends BaseAction {
+    
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        // TODO Auto-generated method stub
+        return super.execute();
+    }
+    
+}
