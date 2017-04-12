e2d79880325ba3cfaca7ab6bb022afb491b30a3b
CongBao <bao_cong@outlook.com>
2017-04-12 12:23:42 +0100

    add search action

---
+++ MovieRecommenderSystem/src/online/mrsys/movierecommender/action/SearchAction.java
@@ -0,0 +1,13 @@
+package online.mrsys.movierecommender.action;
+
+import online.mrsys.movierecommender.action.base.BaseAction;
+
+public class SearchAction extends BaseAction {
+
+    @Override
+    public String execute() throws Exception {
+        // TODO Auto-generated method stub
+        return super.execute();
+    }
+    
+}
